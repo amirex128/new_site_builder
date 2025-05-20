5621ad1c3d377defb40086984bc4ea899792dfb7
Amir.Shirdeli <amir.shirdeli@zoodfood.com>
2025-05-20 20:50:26 +0330

    improve validation system

--- src/internal/application/dto/ticket/models.go
+++ src/internal/application/dto/ticket/models.go
@@ -2,7 +2,7 @@ package ticket
 
 // CommentCommand represents a comment on a ticket
 type CommentCommand struct {
-	Content      *string       `json:"content" validate:"required_text=1 2000"`
-	RespondentID *int64        `json:"respondentId" validate:"required,gt=0"`
-	Media        []interface{} `json:"media,omitempty" validate:"omitempty"`
+	Content      *string       `json:"content" validate:"required_text=1 2000" nameFa:"محتوا"`
+	RespondentID *int64        `json:"respondentId" validate:"required,gt=0" nameFa:"شناسه پاسخگو"`
+	Media        []interface{} `json:"media,omitempty" validate:"omitempty" nameFa:"رسانه"`
 }
